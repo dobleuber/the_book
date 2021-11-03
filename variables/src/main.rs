5419441676a1939bc2cfcc027fdd55cc2c9bccf6
Wbert Adrian Castro Vera <wcastrov@perficient.com>
2021-11-03 18:03:16 -0500

    Initial commit

---
+++ variables/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
+
+   
+    let tup = (500, 6.4, 1);
+    
+    let (x, y, z) = tup;
+    
+    println!("The value of y is: {} {} {}", x, y, z);
+    println!("The value of y is: {}", tup.2);
+    
+    
+}
+
