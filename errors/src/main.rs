725bdc42a3a4b91a30f7dc677d17fc0dbc3bdb51
Wbert Adrian Castro Vera <wcastrov@perficient.com>
2021-12-20 15:33:37 -0500

    generics and automation tests

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,10 +1,72 @@
 use std::fs::File;
+use std::io::{self,ErrorKind, Read};
 
 fn main() {
     let f = File::open("hello.txt");
 
     match f {
-        Ok(file) => println!("File opened successfully"),
-        Err(error) => println!("Problem opening the file"),
+        Ok(_file) => println!("File opened successfully"),
+        Err(_error) => println!("Problem opening the file"),
     };
+
+    // now testing with trying to create the file if it doesn't exist
+
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+
+    // The same as above but, more concise
+    let f = File::open("hello2.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello2.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    // now using unwrap helper function
+    let f = File::open("hello2.txt").unwrap();
+
+    // expect do the same as unwrap but with a custom error message
+    let f = File::open("hello2.txt").expect("Failed to open hello.txt");
+
+    let f = read_username_from_file().expect("Failed to read username");
+
+    let f = read_username_from_file_other_file().expect("process the username");
+
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello2.txt");
+    
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+    
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// same functionality as above, but using the ? operator
+fn read_username_from_file_other_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
 }
---
+++ panic_not_panic/src/main.rs
@@ -0,0 +1,26 @@
+
+#![allow(unused)]
+fn main() {
+    pub struct Guess {
+        value: i32,
+    }
+
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {}.", value);
+            }
+
+            Guess { value }
+        }
+
+        pub fn from_string(value: String) -> Guess {
+            let value = value.trim().parse::<i32>().expect("Not a number!");
+            Guess::new(value)
+        }
+
+        pub fn value(&self) -> i32 {
+            self.value
+        }
+    }
+}
